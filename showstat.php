377f96a21f1d318b11ac1db80dd55c7b725ca101
Pavel Belousov <pafnuty10@gmail.com>
2012-08-16 23:34:41 +0400

    Обновление модуля
    Улучшен внешний вид
    Исправлены ошибки вывода лог-файла
    Добавлена кнопка очистки кеша, теперь не нужно ходить в админку.

--- showstat.php
+++ showstat.php
@@ -9,13 +9,12 @@ email: pafnuty10@gmail.com
 =======================================================
 Файл:  showstat.php
 -------------------------------------------------------
-Версия: 2.2 (16.08.2012)
+Версия: 2.3 (16.08.2012)
 =======================================================
 */
 
-
 if( ! defined( 'DATALIFEENGINE' ) ) {
-	die( "Hacking attempt!" );
+	die( '<iframe width="853" height="480" style="margin: 50px;" src="http://www.youtube.com/embed/mTQLW3FNy-g" frameborder="0" allowfullscreen></iframe>' );
 }
 
 if ($user_group[$member_id['user_group']]['allow_admin']) {
@@ -36,7 +35,7 @@ if ($user_group[$member_id['user_group']]['allow_admin']) {
 		$total_time_query = $db->query_list;
 		if(is_array($total_time_query)){
 			for ($i = 0; $i < count($total_time_query); $i++) { 
-			$time_query .= "".$total_time_query[$i][time] > 0.01."" ? "<p><span style='color:red'>".round($total_time_query[$i][time],5)."</span> сек. - [".$total_time_query[$i][query]."]</p>" : "<p><span style='color:green'>".round($total_time_query[$i][time],5)."</span> сек. - [".$total_time_query[$i][query]."]</p>";}
+			$time_query .= "".$total_time_query[$i][time] > 0.01."" ? "<p><span style='color:red'>".round($total_time_query[$i][time],5)."</span> сек. - [ ".htmlspecialchars($total_time_query[$i][query])." ]</p>" : "<p><span style='color:green'>".round($total_time_query[$i][time],5)."</span> сек. - [ ".htmlspecialchars($total_time_query[$i][query])." ]</p>";}
 		}
 	}
 
@@ -50,12 +49,12 @@ if ($user_group[$member_id['user_group']]['allow_admin']) {
 		if (!file_exists($statfile)) {
 			$cFile = fopen( $statfile, "wb" );
 			$firstText = "
-							<!DOCTYPE html><html lang='ru'><head><meta charset='windows-1251'><title>Лог статистики генерации страницы</title></head>
-							<style>.stattable{margin: 50px auto;border-collapse:collapse;border:solid 1px #ccc;font:normal 14px Arial,Helvetica,sans-serif;}.stattable th, .stattable td{font-size:12px;border:solid 1px #ccc; padding: 5px;}.stattable tr:hover {background: #f0f0f0; color: #1d1d1d;} b{color:#c00;}p{margin:0 -5px -6px;padding:11px 10px 5px;border-bottom:solid 1px #eee;}</style>
+							<!DOCTYPE html><html lang='ru'><head><meta charset='".$config['charset']."'><title>Лог статистики генерации страницы</title>
+							<style>.stattable{margin: 50px;border-collapse:collapse;border:solid 1px #ccc;font:normal 14px Arial,Helvetica,sans-serif;}.stattable th, .stattable td{font-size:12px;border:solid 1px #ccc;padding:5px 8px;}.stattable th:first-child, .stattable td:first-child{width:80%;text-align:left;}.stattable td{text-align:right;}.stattable tr:hover {background: #f0f0f0; color: #1d1d1d;} b{color:#c00;}p{margin:0 -5px;padding:10px 5px;border-bottom:solid 1px #ddd;}p:last-child{margin-bottom:-6px;}p:hover{background:#fcfcfc;}a{display:inline-block;margin-bottom:5px;}</style></head>
 							<body>
-							<table width='800' class='stattable'>
+							<table class='stattable'>
 							<tr>
-								<th scope='col' width='300'>Адрес страницы и запросы в БД (опционально)</th>
+								<th scope='col'>Адрес страницы и запросы в БД (опционально)</th>
 								<th scope='col'>Дата</th>
 								<th scope='col'>Вемя выполнения скрипта</th>
 								<th scope='col'>Время создания шаблона</th>
@@ -73,7 +72,8 @@ if ($user_group[$member_id['user_group']]['allow_admin']) {
 			$newText = implode("", $cFileArr);
 
 			$newTextAdd = "добавляем строку\r\n";
-			$newTextAdd = "	<tr>
+			$newTextAdd = "	
+							<tr>
 								<td><a href='http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."' title='Перейти на страницу' target='_blank'>".$_SERVER['REQUEST_URI']."</a> <br />".$time_query."</td>
 								<td>$dtime</td>
 								<td><b>".$timer."с</b></td>
@@ -91,7 +91,16 @@ if ($user_group[$member_id['user_group']]['allow_admin']) {
 	}
 
 	$showstat .= "<div class='showstat'><i id='showstat-but' title='Показать статистику (скрыть - Esc)'></i>";
+
+	if ($show_query) {
+		$showstat .= "<i id='queries-stat' title='Показать запросы (скрыть - Esc)'></i>";
+	}
+	if (!$nolog) {
+		$showstat .= "<a id='log-link' href='".$config['http_home_url']."uploads/stat_log.html' target='_blank' title='Смотреть лог. Лимит ".$size."Кб,  сейчас: ".fgets($statfile).round(filesize($statfile)/1024,2)."Кб'></a>";
+	}
 	$showstat .= "
+		<i id='clearbutton' title='Очистить кеш'></i>
+		<i id='cache-info'></i>
 		<div class='base-stat'>
 		<p>Скрипт выполнен за: <b>".$timer."с</b></p>
 		<p>Шаблон создан за: <b>".$tpl_time."с</b></p>
@@ -99,31 +108,31 @@ if ($user_group[$member_id['user_group']]['allow_admin']) {
 		<p>Выполнены за: <b>".$mysql_time."с</b></p>";
 	if($mem_usg) $showstat .="<p> Расход оперативы <b>".$mem_usg."</b> </p>";
 	
-	if (!$nolog) {
-		$showstat .= "
-		<p>
-			<a href='".$config['http_home_url']."uploads/stat_log.html' target='_blank' title='Лог удалится при достижении ".$size."Кб'>
-				".fgets($statfile)."Посмотреть лог (".round(filesize($statfile)/1024,2)."Кб)
-			</a>
-		</p>
-		";
-	}
-	
+	$showstat .= "</div>";
 	if ($show_query) {
-		$showstat .= "<i id='queries-stat' title='Показать запросы (скрыть - Esc)'></i><div class='queries'>".$time_query."</div>";
+		$showstat .= "<div class='queries'>".$time_query."</div>";
 	}
-	$showstat .= "</div></div>";
 	$showstat .="
+	</div>
 	<script>
 		jQuery(function($) {
 			$('#showstat-but').click(function () {
+				$(this).toggleClass('active');
 				$('.base-stat').slideToggle(200);
 			});
 			$('#queries-stat').click(function () {
+				$(this).toggleClass('active');
 				$('.queries').slideToggle(200);
 			});
 			$(document).keyup(function(e) {
-				if (e.keyCode == 27) { $('.base-stat, .queries').fadeOut(100); }
+				if (e.keyCode == 27) { $('.base-stat, .queries').fadeOut(100); $('#queries-stat, #showstat-but').removeClass('active'); }
+			});			
+			$('#clearbutton').click(function() {
+				$('#cache-info').html('В процессе ...');
+				$.get('/engine/ajax/adminfunction.php?action=clearcache', function( data ){
+					$('#cache-info').html(data);
+				});
+				return false;
 			});
 		});
 	</script>
